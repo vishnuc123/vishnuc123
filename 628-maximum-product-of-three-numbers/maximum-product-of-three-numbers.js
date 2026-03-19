8a084996a15337af1047b880deb5ad517798cac6
VISHNU C <158842157+vishnuc123@users.noreply.github.com>
2026-03-19 23:40:01 +0530

    Time: 35 ms (64.80%) | Memory: 59.1 MB (57.40%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
@@ -3,21 +3,13 @@
  * @return {number}
  */
 var maximumProduct = function (nums) {
-    let sort = nums.sort((a, b) => {
-        if (a < b) return -1;
-        else if (a > b) return 1;
-        else return 0;
-    });
+    nums.sort((a, b) => a - b);
+    let n = nums.length;
 
-    let product1 = 1;
-
-  
-    for (let i = sort.length - 1; i >= sort.length - 3; i--) {
-        product1 *= sort[i];
-    }
-    let product2 = sort[0] * sort[1] * sort[sort.length - 1];
-
-    return Math.max(product1, product2);
+    return Math.max(
+        nums[n - 1] * nums[n - 2] * nums[n - 3],
+        nums[0] * nums[1] * nums[n - 1]
+    )
 
 
 };
